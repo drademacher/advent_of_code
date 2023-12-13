5ce81eaf6dc5f20f96b81c4d2a04a29eb8be1dd2
drademacher <danny.rademacher@gmail.com>
2023-12-13 06:18:46 +0100

    Solve day 12

--- src/year2023/Day12.kt
+++ src/year2023/Day12.kt
@@ -6,10 +6,10 @@ fun main() {
     val input = parseInput(readLines("2023", "day12"))
     val testInput = parseInput(readLines("2023", "day12_test"))
 
-    check(part1(testInput) == 21)
+    check(part1(testInput) == 21L)
     println("Part 1:" + part1(input))
 
-    check(part2(testInput) == 525152)
+    check(part2(testInput) == 525152L)
     println("Part 2:" + part2(input))
 
 }
@@ -18,69 +18,83 @@ private fun parseInput(input: List<String>): List<String> {
     return input
 }
 
-private fun part1(input: List<String>): Int {
+private fun part1(input: List<String>): Long {
     val arrangements = input.map { row ->
-        val springMap = row.takeWhile { !it.isWhitespace() }
+        memory = HashMap()
+
+        val visualMap = row.takeWhile { !it.isWhitespace() }
         val groups = row.dropWhile { !it.isDigit() }.split(",").map { it.toInt() }
 
-        val placeableHash = groups.sum() - springMap.count { it == '#' }
-        val placeableDots = springMap.count { it == '?' } - placeableHash
-
-        recurse(springMap, groups, placeableHash, placeableDots)
+        calculateNumberOfArrangements(visualMap, 0, groups)
     }
 
     return arrangements.sum()
-
 }
 
-private fun recurse(s: String, groups: List<Int>, placeableHash: Int, placeableDots: Int): Int {
-    if (placeableHash == 0 && placeableDots == 0) {
-        return if (magic(s, groups)) 1 else 0
-    }
+private fun part2(input: List<String>): Long {
+    val arrangementsWithUnfoldedInput = input.map { row ->
+        memory = HashMap()
 
-    var res = 0
-    if (placeableDots > 0) {
-        res += recurse(s.replaceFirst('?', '.'), groups, placeableHash, placeableDots - 1)
-    }
-
-    if (placeableHash > 0) {
-        res += recurse(s.replaceFirst('?', '#'), groups, placeableHash - 1, placeableDots)
-    }
-
-    return res
-}
-
-private fun magic(s: String, groups: List<Int>): Boolean {
-    var mutableString = s.dropWhile { it != '#' }.dropLastWhile { it != '#' } + "."
-
-    for (group in groups) {
-
-        val groupOfHash = mutableString.takeWhile { it == '#' }.length
-        val groupOfDotAfterwards = mutableString.dropWhile { it == '#' }.takeWhile { it == '.' }.length
-
-        if (groupOfHash != group || groupOfDotAfterwards == 0) {
-            return false
-        }
-
-        mutableString = mutableString.drop(groupOfHash + groupOfDotAfterwards)
-    }
-
-    return true
-}
-
-private fun part2(input: List<String>): Int {
-    val newInput = input.map {row ->
-        val springMap = row.takeWhile { !it.isWhitespace() }
+        val visualMap = row.takeWhile { !it.isWhitespace() }
         val groups = row.dropWhile { !it.isDigit() }.split(",").map { it.toInt() }
 
-        val newMap = (1..5).map { springMap }.joinToString("?")
-        val newGroups = (1..5).flatMap { groups }
+        val unfoldedVisualMap = (1..5).joinToString("?") { visualMap }
+        val unfoldedGroups = (1..5).flatMap { groups }
 
-        val placeableHash = newGroups.sum() - newMap.count { it == '#' }
-        val placeableDots = newMap.count { it == '?' } - placeableHash
-
-        recurse(newMap, newGroups, placeableHash, placeableDots)
+        calculateNumberOfArrangements(unfoldedVisualMap, 0, unfoldedGroups)
     }
 
-    return newInput.sum()
+    return arrangementsWithUnfoldedInput.sum()
+}
+
+private var memory = HashMap<Pair<Int, List<Int>>, Long>()
+
+private fun calculateNumberOfArrangements(visualMap: String, currentIndex: Int, currentGroups: List<Int>): Long {
+    if (currentIndex == visualMap.length && currentGroups.isEmpty()) {
+        return 1
+    } else if (currentIndex >= visualMap.length) {
+        return 0
+    }
+
+    if (memory[Pair(currentIndex, currentGroups)] != null) {
+        return memory[Pair(currentIndex, currentGroups)]!!
+    }
+
+    val result = when (visualMap[currentIndex]) {
+        '.' -> calculateForCurrentIsDot(visualMap, currentIndex, currentGroups)
+        '#' -> calculateForCurrentIsHash(visualMap, currentIndex, currentGroups)
+        '?' -> calculateForCurrentIsDot(visualMap, currentIndex, currentGroups) + calculateForCurrentIsHash(visualMap, currentIndex, currentGroups)
+        else -> throw IllegalStateException("s[current]=${visualMap[currentIndex]} is illegal")
+    }
+
+    memory[Pair(currentIndex, currentGroups)] = result
+
+    return result
+}
+
+private fun calculateForCurrentIsDot(visualMap: String, currentIndex: Int, currentGroups: List<Int>): Long {
+    return calculateNumberOfArrangements(visualMap, currentIndex + 1, currentGroups)
+}
+
+private fun calculateForCurrentIsHash(visualMap: String, currentIndex: Int, currentGroups: List<Int>): Long {
+    if (currentGroups.isEmpty()) {
+        return 0
+    }
+
+    val firstGroup = currentGroups.first()
+
+    if (currentIndex + firstGroup > visualMap.length) {
+        return 0
+    }
+
+    val nextGroupIsNotDot = visualMap.drop(currentIndex).take(firstGroup).all { it == '#' || it == '?' }
+    val isLastGroup = currentGroups.size == 1
+    val nextSymbolIsNotHash = currentIndex + firstGroup == visualMap.length || visualMap[currentIndex + firstGroup] != '#'
+
+    if (nextGroupIsNotDot && (isLastGroup || nextSymbolIsNotHash)) {
+        val offset = if (isLastGroup) 0 else 1
+        return calculateNumberOfArrangements(visualMap, currentIndex + firstGroup + offset, currentGroups.drop(1))
+    }
+
+    return 0
 }
